eff953a1c8904d4e74d8c2e34c9f8c4a4ba65a90
Quan Le <meou2731999@gmail.com>
2024-05-20 15:27:43 +0700

    Updated api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,40 +1,48 @@
-// lib/api.ts
 import { Hotel, PriceResponse, SupportedCurrency } from "../types";
 
 export async function fetchHotelData(
   currency: SupportedCurrency
 ): Promise<Hotel[]> {
-  const hotelResponse = await fetch(
-    "https://interview-api.vercel.app/api/hotels/tokyo"
-  );
-  const hotels: Hotel[] = await hotelResponse.json();
+  try {
+    const hotelResponse = await fetch(
+      "https://interview-api.vercel.app/api/hotels/tokyo"
+    );
+    const hotels: Hotel[] = await hotelResponse.json();
 
-  const priceResponse = await fetch(
-    `https://interview-api.vercel.app/api/hotels/tokyo/1/${currency}`
-  );
-  const priceDataList: PriceResponse[] = await priceResponse.json();
+    const priceResponse = await fetch(
+      `https://interview-api.vercel.app/api/hotels/tokyo/1/${currency}`
+    );
 
-  // Map price data to hotels
-  for (const hotel of hotels) {
-    const priceData = priceDataList.find((item) => item.id === hotel.id);
-    if (priceData) {
-      hotel.price = priceData.price;
-      hotel.competitors = priceData.competitors;
-      hotel.taxes_and_fees = priceData.taxes_and_fees;
+    // Check for HTTP status code 404 (Not Found)
+    if (!priceResponse.ok) {
+      // Update hotels where price is not available
+      for (const hotel of hotels) {
+        hotel.price = undefined;
+        hotel.competitors = undefined;
+        hotel.taxes_and_fees = undefined;
+      }
+
+      return hotels;
     }
+
+    const priceDataList: PriceResponse[] = await priceResponse.json();
+
+    // Map price data to hotels
+    for (const hotel of hotels) {
+      const priceData = priceDataList.find((item) => item.id === hotel.id);
+      hotel.price = priceData?.price;
+      hotel.competitors = priceData?.competitors;
+      hotel.taxes_and_fees = priceData?.taxes_and_fees;
+    }
+
+    // Push hotels with "Rates unavailable" to the bottom of the list
+    const sortedHotels = hotels.sort((a, b) =>
+      a.price ? (b.price ? 0 : -1) : b.price ? 1 : 0
+    );
+
+    return sortedHotels;
+  } catch (error: any) {
+    console.error("Error fetching hotel data:", error.message);
+    return []; // Return an empty array in case of an error
   }
-
-  // Push hotels with "Rates unavailable" to the bottom of the list
-  const sortedHotels = hotels.sort((a, b) => {
-    // Unavailable hotels should be pushed to the bottom
-    if (!a.price && b.price) {
-      return 1;
-    }
-    if (a.price && !b.price) {
-      return -1;
-    }
-    return 0;
-  });
-
-  return sortedHotels;
 }
