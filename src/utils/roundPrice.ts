c766f118f2d040f6fbcc0834a65b73093dab7b0f
Quan Le <meou2731999@gmail.com>
2024-05-20 14:23:36 +0700

    Update roundPrice

--- src/utils/roundPrice.ts
+++ src/utils/roundPrice.ts
@@ -3,10 +3,10 @@ import type { SupportedCurrency } from "@/types";
 export const roundPrice = (
   price: number,
   currency: SupportedCurrency
-): number => {
+): string => {
   if (["KRW", "JPY", "IDR"].includes(currency)) {
-    return Math.round(price / 100) * 100;
+    return (Math.round(price / 100) * 100).toLocaleString();
   } else {
-    return Math.round(price);
+    return Math.round(price).toString();
   }
 };
