b84b6a6e1d744645d93cce15b657aa0e247bc436
Quan Le <meou2731999@gmail.com>
2024-07-23 02:58:12 +0700

    Add icons

---
+++ src/assets/PlusIcon.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type Props = {
+  fill?: string;
+};
+
+const PlusIcon: React.FC<Props> = ({ fill = "#8F47FF" }: Props) => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.99992 3.83325V17.1666M16.6666 10.4999L3.33325 10.4999"
+        stroke={fill}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default PlusIcon;
---
+++ src/assets/XIcon.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type Props = {
+  fill?: string;
+};
+
+const XIcon: React.FC<Props> = ({ fill = "#D52F25" }: Props) => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 24L24 8M8 8L24 24"
+        stroke={fill}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default XIcon;
