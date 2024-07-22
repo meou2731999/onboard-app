bf3e90b4a0866a59766188acdda297958c8a004b
Quan Le <meou2731999@gmail.com>
2024-07-23 02:59:44 +0700

    Add textfield

---
+++ src/components/Field/Field.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type Props = {
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+export const Field: React.FC<Props> = ({ onChange }: Props) => {
+  return (
+    <div className="border-[0.5px] h-[46px] border-solid w-full flex items-center overflow-hidden rounded-lg relative border-gray-scale-60 ">
+      <div className="flex h-full items-center px-2.5 bg-gray-scale-20">
+        <div className="text-body-large-regular text-gray-scale-60">
+          https://
+        </div>
+      </div>
+      <input
+        className="relative text-body-medium-regular text-gray-scale-40 w-full h-full p-2  bg-gray-scale-15 rounded-e-lg focus:outline-none"
+        placeholder="input URL"
+        onChange={onChange}
+      />
+    </div>
+  );
+};
