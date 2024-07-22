1d4ec0099a7800a1c2a77e2a6df9971faa57e133
Quan Le <meou2731999@gmail.com>
2024-07-22 18:34:10 +0700

    Update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,9 +21,56 @@ const config: Config = {
         primary: "#8F47FF",
         disable: "#D4BBF7",
         neutral: "#1D1D1D",
+        secondary: "#F1ECFE",
       },
       fontFamily: {
         fontFamily: "Be_Vietnam_Pro",
+      },
+      fontSize: {
+        "body-large-medium": [
+          "16px",
+          { lineHeight: "1.5", letterSpacing: "0.08px", fontWeight: "500" },
+        ],
+        "body-large-regular": [
+          "16px",
+          { lineHeight: "1.5", letterSpacing: "0.08px", fontWeight: "400" },
+        ],
+        "body-large-semibold": [
+          "16px",
+          { lineHeight: "1.5", letterSpacing: "0.08px", fontWeight: "600" },
+        ],
+        "body-medium-regular": [
+          "14px",
+          { lineHeight: "1.5", letterSpacing: "0.035px", fontWeight: "400" },
+        ],
+        "body-medium-semibold": [
+          "14px",
+          { lineHeight: "1.5", letterSpacing: "0.035px", fontWeight: "600" },
+        ],
+        "caption-regular": [
+          "10px",
+          { lineHeight: "1.5", letterSpacing: "0.05px", fontWeight: "400" },
+        ],
+        "caption-semibold": [
+          "10px",
+          { lineHeight: "1.5", letterSpacing: "0.05px", fontWeight: "600" },
+        ],
+        "display-small-semibold": [
+          "36px",
+          { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "600" },
+        ],
+        "heading-medium-semibold": [
+          "28px",
+          { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "600" },
+        ],
+        "label-large-bold": [
+          "14px",
+          { lineHeight: "1.5", letterSpacing: "0.014px", fontWeight: "400" },
+        ],
+        "title-small-regular": [
+          "18px",
+          { lineHeight: "1.5", letterSpacing: "0.014px", fontWeight: "400" },
+        ],
       },
     },
   },
